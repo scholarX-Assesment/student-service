9e79bfcb22beff74dc250b5074f905680e942177
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-12-15 00:36:45 +0530

    feat: configure environment variables for database connection and update .gitignore

--- database/database.py
+++ database/database.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:ahys$532sF@scholarx.c12yaew4cfgj.us-east-1.rds.amazonaws.com:5432/student_db"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
